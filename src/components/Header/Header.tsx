7303059b36e4ee6d674faf07af8eb0c9b731408d
rengated <kazievserg@gmail.com>
2023-12-16 21:07:52 +0300

    items-center header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,10 +8,10 @@ export const Header: FC = () => {
   const { currentTheme, toggleTheme } = useContext(Theme);
 
   return (
-    <section className=" w-full h-screen border-b-2 border-zinc-600  css-current-storage">
+    <section className=" w-full h-screen border-b-2 border-zinc-600 ">
       <div className="container align-center justify-center  mx-auto my-0 w-full h-full flex flex-col px-4">
         <header
-          className={`flex mb-auto sticky top-0 py-10 ${
+          className={`flex mb-auto sticky top-0 py-10 items-center ${
             currentTheme == "black" ? "bg-black " : "bg-white"
           } `}>
           <h2 className="font-extrabold text-2xl">RENGATED DEV.</h2>
