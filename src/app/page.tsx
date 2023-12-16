97c4d5f05e25382facc6f4f1e09a5265af43bb0a
rengated <kazievserg@gmail.com>
2023-12-16 14:10:01 +0300

    fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import { Header } from "@/components/Header/Header";
 import { Projects } from "@/components/Projects/Projects";
 import { About } from "@/components/About/About";
-import "react-multi-carousel/lib/styles.css";
-import { Contact } from "@/components/Contact/Contact";
 import { Footer } from "@/components/Footer/Footer";
 
 export default function Home() {
